ebcb9c8b64bc87a4af79257f95c8c51d562c1738
MauricioGoulartOliveira <snarfbass36@hotmail.com>
2024-03-11 17:04:07 -0300

    tornando o html semantico

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let numerA = prompt("Digite o primeiro numero");
+/*let numerA = prompt("Digite o primeiro numero");
 let numeroB = prompt("Digite o segundo  numero");
 let operaCao = prompt("Digite a operação a ser realizada");
 
-window.alert("A soma dos dois numeros é:" + eval(numerA + operaCao + numeroB));
+window.alert("A soma dos dois numeros é:" + eval(numerA + operaCao + numeroB));*/
