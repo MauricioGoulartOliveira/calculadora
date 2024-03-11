2bbc8c767426a7342798cf1501bdd36c74949d21
MauricioGoulartOliveira <snarfbass36@hotmail.com>
2024-03-11 10:49:11 -0300

    Exercicio calculadora 4.1 e 4.2

---
+++ script.js
@@ -0,0 +1,5 @@
+let numerA = prompt("Digite o primeiro numero");
+let numeroB = prompt("Digite o segundo  numero");
+let operaCao = prompt("Digite a operação a ser realizada");
+
+window.alert("A soma dos dois numeros é:" + eval(numerA + operaCao + numeroB));
