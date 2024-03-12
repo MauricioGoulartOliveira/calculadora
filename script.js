63b24e52f14b932aa831d2368b48999731dbdc58
MauricioGoulartOliveira <snarfbass36@hotmail.com>
2024-03-12 15:58:40 -0300

    Finalizado desenvolvimento da calculadora
    - Atualizados os arquivos JavaScript e CSS
    - Realizadas outras melhorias e ajustes no código

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-/*let numerA = prompt("Digite o primeiro numero");
-let numeroB = prompt("Digite o segundo  numero");
-let operaCao = prompt("Digite a operação a ser realizada");
+var display = '';
+var operacoes = [];
 
-window.alert("A soma dos dois numeros é:" + eval(numerA + operaCao + numeroB));*/
+function agregarDisplay(elemento) {
+  this.atualizarDisplay(display += elemento);
+}
+
+function calcularOperacao() {
+  //salva a operacao realizada no historico de 'operacoes'
+  operacoes.push(display + '=' + eval(display));
+  this.atualizarDisplay(eval(display));
+}
+
+function limpar() {
+  this.atualizarDisplay('');
+}
+
+function atualizarDisplay(valor) {
+  display = valor;
+  var displayInfo = document.getElementById('display-info');
+  //input
+  //displayInfo.value = '';
+  //span
+  displayInfo.innerHTML = valor;
+}
+
+function mostrarHistorico() {
+  var listaHistorico = document.getElementById('lista-historico');
+
+  //caso o elemento listaHistorico possua algum elemento filho, ele será removido
+  //isso é feito para que não fique 'sujeira' de elementos exibidos anteriormente (duplicados)
+  while(listaHistorico.firstChild){
+    listaHistorico.removeChild(listaHistorico.firstChild);
+  }
+
+  //iteração na lista de 'operacoes' onde, pra cada 'operacao'
+  //será criado um elemento li, seu conteúdo será atualizado com a operacao realizada
+  //e esse elemento será adicionado no elemento 'listaHistorico'
+  operacoes.forEach(operacao => {
+    var li = document.createElement('li');
+    li.innerHTML = operacao;
+    listaHistorico.appendChild(li);
+  });
+}
+
+function limparHistorico() {
+  var listaHistorico = document.getElementById('lista-historico');
+
+  //limpa as 'operacoes' realizadas
+  operacoes = [];
+
+  //caso o elemento 'listaHistorico' possua algum elemento filho, ele será removido
+  while(listaHistorico.firstChild){
+    listaHistorico.removeChild(listaHistorico.firstChild);
+  }
+}
